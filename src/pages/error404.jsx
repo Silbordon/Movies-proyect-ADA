5e478275c4ef2719ef6a8b582272b59fe9419f72
maricaroj <juarezmariana18@gmail.com>
2021-07-26 18:38:42 -0300

    UPDATE/add error page styles and responsive

--- src/pages/error404.jsx
+++ src/pages/error404.jsx
@@ -1,5 +1,16 @@
+import { Button } from 'antd';
+import {Link} from 'react-router-dom'
+import Footer from '../components/Footer';
+import '../index.css'
+
 const Error404 = () =>{
-    return "error 404"
+    return (
+            <div className='error-container'>
+                <div className='error-404'></div>
+                <p>Sorry, the page you visited does not exist.</p>
+                <Link to ="/"> <Button type="primary"> Home</Button></Link>
+            </div>
+    )
 }
 
 export default Error404;
